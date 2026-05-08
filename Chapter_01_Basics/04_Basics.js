f4d82fdf3481e29323f0552c3f83a2524aa00c3d
Harsh Valecha <harsh.valecha@yahoo.com>
2026-05-08 07:57:10 +0530

    08_May_2026

---
+++ Chapter_01_Basics/04_Basics.js
@@ -0,0 +1,15 @@
+var v = 10;
+
+// fir priting use console.log
+console.log(v);
+
+/*
+var - keyword
+v - identifier
+= - operator
+10 - literal
+; - end of statement
+*/
+
+v = 12;
+console.log(v); // value updated
---
+++ Chapter_01_Basics/05_identifier_rules.js
@@ -0,0 +1,21 @@
+// variable namings -- all name below are possible
+var $ = 12;
+var _k= 12;
+var p = 23;
+
+var abc123 = 23; //possible
+// var 123abc = 45; -- not possible
+
+
+// both variables names are different 
+var Name = 'pramod';
+var name = 'kamlesh';
+
+// unicode character is possible
+// in between dollar is also possible
+
+// best practice is to use lower case variable namings
+// use camelcase
+
+var firstName = "Kamlesh";
+console.log(firstName);
