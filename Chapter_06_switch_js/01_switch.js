27d337857023ff864154dd280426e35411f6d2c2
Harsh Valecha <harsh.valecha@yahoo.com>
2026-05-30 11:26:08 +0530

    added switch case exercise

---
+++ Chapter_06_switch_js/01_switch.js
@@ -0,0 +1,13 @@
+let name = "Bhoal";
+
+switch (name) {
+    case "Hola":
+        console.log("Hola there");
+        break;
+    case "Bhola":
+        console.log("Bhola there");
+        break;
+    default:
+        console.log("Default here");
+        break;
+}
