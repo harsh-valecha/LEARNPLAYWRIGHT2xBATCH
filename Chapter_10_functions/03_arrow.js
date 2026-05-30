499fc2069243fb147dddb0676c6ebf2343ae8f56
Harsh Valecha <harsh.valecha@yahoo.com>
2026-05-30 11:19:20 +0530

    added some more arrow function

--- Chapter_10_functions/03_arrow.js
+++ Chapter_10_functions/03_arrow.js
@@ -2,4 +2,8 @@
 const greet = (name) => "Hello " + name;
 console.log(greet("Kirpal"));
 
+const doubleIt = n => n * 2;
+console.log(doubleIt(20));
 
+const printIt = name => console.log(name);
+printIt("Jaspal");
